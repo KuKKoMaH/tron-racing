4a826f2c4ff93c74a52156253426b8a659dd83ac
KuKKoMaH <kukkoman@gmail.com>
2016-08-16 23:14:31 +0300

    refactor server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var express = require('express');
 var peer = require('peer');
 
+
 var ExpressPeerServer = peer.ExpressPeerServer;
 var port = process.env.PORT || 8080;
 
@@ -9,3 +10,13 @@ var app = express();
 var server = app.listen(port);
 app.use(express.static(__dirname + '/../dist'));
 app.use('/peers', ExpressPeerServer(server, {}));
+
+if (process.env.NODE_ENV != 'production') {
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var webpackHotMiddleware = require('webpack-hot-middleware');
+  var webpack = require('webpack');
+  var config = require(__dirname + '/../webpack.config.js');
+  var compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
